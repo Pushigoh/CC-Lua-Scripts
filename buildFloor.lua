bd11c972c5892e89ab5fb1ed323263f3793b0532
pushigoh <ajbacc@gmail.com>
2023-10-29 14:11:02 -0500

    Update buildFloor.lua

--- buildFloor.lua
+++ buildFloor.lua
@@ -1,2 +1,25 @@
-for i = 0, 10 do
+-- buildFloor.lua
+os.loadAPI("TurtleNav.lua")
+width = 10
+length = 10
+
+
+turtle.turnRight()
+for i = 0, length do
+    for x = 0, length do
+        turtle.placeDown()
+        TurtleNav.move("forward")
+    end
+    if i % 2 = 0 then
+        turtle.turnLeft()
+        TurtleNav.move("forward")
+        turtle.turnLeft()
+    else
+        turtle.turnRight()
+        TurtleName.move("forward")
+        turtle.turnRight()
+    end
+    
+    
+
     
